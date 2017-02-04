c313b1108e677ca17463d1e17d67d2d363802492
GaganTut <gstut@live.com>
2017-02-03 20:35:03 -1000

    Capitalized Yoda because he deserves the RESPECT

--- exercise.js
+++ exercise.js
@@ -31,9 +31,9 @@ console.log(kanyeWants);
 
 /*3. Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
 
-var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
-yoda.reverse();
-console.log(yoda);
+var Yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
+Yoda.reverse();
+console.log(Yoda);
 
 /*The array.shift() removes the first element from an array and returns that element*/
 
